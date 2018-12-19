367a858772c65269347a49adf19809e06e085f64
ardeleanraz <ardeleanrazvan111@yahoo.com>
2018-12-19 16:53:15 +0200

    update

---
+++ build/precache-manifest.8b4f6ba7bfd7e501d3f25e69a3eba43b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "94ee5ca86760189846542bd671986bcb",
+    "url": "/static/media/road1.94ee5ca8.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "969f1e8fc287c2bc62c0",
+    "url": "/static/js/main.969f1e8f.chunk.js"
+  },
+  {
+    "revision": "4d4949a9a9f903a5cc4a",
+    "url": "/static/js/1.4d4949a9.chunk.js"
+  },
+  {
+    "revision": "969f1e8fc287c2bc62c0",
+    "url": "/static/css/main.88dda46a.chunk.css"
+  },
+  {
+    "revision": "0f62fd50cb5b955b874daa3a30679532",
+    "url": "/index.html"
+  }
+];
